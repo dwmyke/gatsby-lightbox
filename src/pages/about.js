81a4e0aec39422fbb506862f56b863e793e420f9
Mikel-Jon West <dwmyke@gmail.com>
2019-02-12 22:51:01 -0800

    created About page

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const About = () => (
+    <Layout>
+    <div>
+        <h1>Mikel-Jon (Mike) West</h1>
+        <p>
+            I'm a guy building a website. More to come.
+        </p>
+    </div>
+    </Layout>
+)
+
+export default About
